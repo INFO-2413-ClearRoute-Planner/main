1cff83d11ebca0418e7d53f85aff7655e4780f84
Roman <romanumo1@gmail.com>
2025-07-16 16:50:50 -0700

    Exploration

--- script.js
+++ script.js
@@ -1,50 +1,18 @@
 //Map UI
-var map = L.map('map').setView([51.134685, 71.411362], 15);
+var map = L.map('map').setView([49.285996,-123.040412], 15);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-
-//Marker Handling 
-let startMarker = null;
-let endMarker = null;
-
-map.on('click', function(e){
-    //Receive Coordinates on click
-    var coord = e.latlng.toString().split(',');
-    var lat = coord[0].split('(');
-    var lng = coord[1].split(')');
-
-    //Set up Marker
-    if(startMarker == null)
-    {
-        startMarker = L.marker([lat[1], lng[0]]).addTo(map);
-        document.getElementById("start").textContent = coord;
-    }
-    else if(endMarker == null) 
-    {
-        endMarker = L.marker([lat[1], lng[0]]).addTo(map);
-        document.getElementById("end").textContent = coord;
-    }
-});
-
-document.getElementById("clearDir").onclick = clearDirection
-
-function clearDirection()
-{
-    if(startMarker) 
-    {
-        map.removeLayer(startMarker);
-        startMarker = null;
-        document.getElementById("start").textContent = "";
-    }
-
-    if(endMarker) 
-    {
-        map.removeLayer(endMarker);
-        endMarker = null;
-        document.getElementById("end").textContent = "";
-    }
-}
+L.Routing.control({
+waypoints: [
+    L.latLng(49.316882,-123.042128),
+    L.latLng(49.262722,-123.027022)
+],
+router: L.Routing.graphHopper(false, {
+    serviceUrl: 'http://localhost:8989/route'  // Point to your GraphHopper server
+}),
+routeWhileDragging: true
+}).addTo(map);
