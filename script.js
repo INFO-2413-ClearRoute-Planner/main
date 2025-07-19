376f2f4e58b81cfdfeb2f6c180e3a3b8b52c2503
Roman <romanumo1@gmail.com>
2025-07-19 15:54:15 -0700

    Integrated Backend

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Map UI
-var map = L.map('map').setView([51.1605, 71.4704], 10);
+var map = L.map('map').setView([52.22461607040788, 20.99271139468625], 7);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -8,10 +8,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 L.Routing.control({
 waypoints: [
-    L.latLng(51.1605, 71.4704),  // Astana
-    L.latLng(49.8047, 73.1094)   // Karaganda
+    L.latLng(52.26322137056714, 21.015384884035132),  // Warsaw
+    L.latLng(52.26044579171676, 21.016167448266224)  
 ],
-router: L.Routing.graphHopper(false, {
+router: new L.Routing.GraphHopper(false, {
     serviceUrl: 'http://localhost:8989/route',
     urlParameters: {
       profile: 'truck1',
@@ -21,14 +21,33 @@ router: L.Routing.graphHopper(false, {
       custom_model: {
         distance_influence: 1,
         priority: [
-          { if: "hgv == NO", multiply_by: "0" },
-          { if: "max_height < 4.2", multiply_by: "0" }
+          { if: "max_height < 5", multiply_by: "0" }
         ],
         speed: [
-          { if: "true", limit_to: "95" }
+          { if: "true", limit_to: "80" }
         ]
       }
     }
   }),
 routeWhileDragging: true
 }).addTo(map);
+
+
+// fetch('http://localhost:8989/route?profile=truck1&ch.disable=true', {
+//   method: 'POST',
+//   headers: { 'Content-Type': 'application/json' },
+//   body: JSON.stringify({
+//     points: [
+//       [52.263221, 21.015385],
+//       [52.260446, 21.016167]
+//     ],
+//     custom_model: {
+//       distance_influence: 1,
+//       priority: [{ if: "true", multiply_by: "0" }],
+//       speed: [{ if: "true", limit_to: "80" }]
+//     }
+//   })
+// })
+// .then(r => r.json())
+// .then(console.log)
+// .catch(console.error);
