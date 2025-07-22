53111dc1fad4ba528d4cc21ec0020c2cba1df0ac
Roman <romanumo1@gmail.com>
2025-07-21 20:42:52 -0700

    Fixed Routing & BC map

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Map UI
-var map = L.map('map').setView([52.22461607040788, 20.99271139468625], 7);
+var map = L.map('map').setView([49.28207317260126, -123.03236951998038], 14);
 
 // Shows the Map
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,50 +7,30 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// Control the Routing
-// Sends GET request to server (8989) with changed profile (WIP)
-L.Routing.control({
-waypoints: [
-    L.latLng(52.26322137056714, 21.015384884035132),  // Warsaw
-    L.latLng(52.26044579171676, 21.016167448266224)  
-],
-router: new L.Routing.GraphHopper(false, {
-    serviceUrl: 'http://localhost:8989/route',
-    urlParameters: {
-      profile: 'truck1',
-      ch: 'false'
-    },
-    requestParameters: {
-      custom_model: {
-        distance_influence: 1,
-        priority: [
-          { if: "max_height < 5", multiply_by: "0" }
-        ],
-        speed: [
-          { if: "true", limit_to: "80" }
-        ]
-      }
+// Request for the route
+fetch('http://localhost:8989/route?ch=false', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    profile: 'truck1',
+    points: [
+      // Here the coordinates have to be swapped
+      [-123.02996285956368, 49.279381755743714], 
+      [-123.05059137742096, 49.31794469596032]
+    ],
+    custom_model: {
+      distance_influence: 1,
+      priority: [{ if: "max_height < 5", multiply_by: "0" }],
+      speed: [{ if: "true", limit_to: "80" }]
     }
-  }),
-routeWhileDragging: true
-}).addTo(map);
-
-
-// fetch('http://localhost:8989/route?profile=truck1&ch.disable=true', {
-//   method: 'POST',
-//   headers: { 'Content-Type': 'application/json' },
-//   body: JSON.stringify({
-//     points: [
-//       [52.263221, 21.015385],
-//       [52.260446, 21.016167]
-//     ],
-//     custom_model: {
-//       distance_influence: 1,
-//       priority: [{ if: "true", multiply_by: "0" }],
-//       speed: [{ if: "true", limit_to: "80" }]
-//     }
-//   })
-// })
-// .then(r => r.json())
-// .then(console.log)
-// .catch(console.error);
+  })
+})
+.then(response => response.json())
+.then(data => {
+  const encoded = data.paths[0].points; // this is the encoded polyline
+  const coords = L.Polyline.fromEncoded(encoded).getLatLngs();
+  L.polyline(coords, { color: 'blue' }).addTo(map);
+})
+.catch(err => {
+  console.error("Routing failed:", err);
+});
