8af6cdc8b6856d3eb96cdcbd45d7b69e2836b610
jlrequinto <jennelyn.requinto@gmail.com>
2025-07-24 16:28:57 -0700

    changes in index.html - Starting from Line 233 register Form 1. rename input id name by removing the "-" e.g register-name change to registerName due to some error when creating js validation script 2. adding span id for js front-end validation
    Changes in script js- Adding front-end validation
    from line 24 _> //Registration Front-end Validation

--- script.js
+++ script.js
@@ -20,3 +20,51 @@ map.on('click', function(e){
     console.log("You clicked the map at latitude: " + lat[1] + " and longitude:" + lng[0]);
     currentMarker = L.marker([lat[1], lng[0]]).addTo(map);
 });
+
+//Registration Front-end Validation
+
+//Validate Full Name
+document.getElementById("registerName").addEventListener("input", function() {
+const name = this.value.trim();
+const error = document.getElementById("nameError");
+
+if (name === "" || !/^[A-Za-z\s]+$/.test(name)) {
+    error.textContent = "Only letters and spaces allowed.";
+} 
+else {
+    error.textContent = "";
+}
+});
+
+//Validate Email
+document.getElementById("registerEmail").addEventListener("input", function() {
+const email = this.value.trim();
+const error = document.getElementById("emailError");
+const pattern = /\S+@\S+\.\S+/;
+
+error.textContent = pattern.test(email) ? "" : "Invalid email format.";
+});
+
+//Validate Password
+document.getElementById("registerPassword").addEventListener("input", function() {
+const password = this.value;
+const error = document.getElementById("passwordError");
+
+// At least 1 upper case and 1 symbol and 8 characters long
+const passwordPattern = /^(?=.*[A-Z])(?=.*[\W_]).{8,}$/;
+if (!passwordPattern.test(password)) {
+    error.textContent = "Password must be at least 8 characters long and include 1 symbol & 1 uppercase  letter. ";
+}
+else {
+    error.textContent = "";
+}
+});
+
+//Validate Confirm Password
+document.getElementById("registerConfirm").addEventListener("input", function() {
+const confirm = this.value;
+const password = document.getElementById("registerPassword").value;
+const error = document.getElementById("confirmPassError");
+
+error.textContent = confirm === password ? "" : "Password do not match.";
+});
